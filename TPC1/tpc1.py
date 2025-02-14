0c043f9d276f4642aaea8c94dd251ec42b90d8a6
guedes674 <a97369@alunos.uminho.pt>
2025-02-14 00:42:14 +0000

    tpc1 final (depois de saber que é para fazer sem regex)

--- TPC1/tpc1.py
+++ TPC1/tpc1.py
@@ -1,20 +1,32 @@
-import re
+import sys
 
-def somador_on_off(texto):
-    ac = []
-    a_somar = True
+text = "Hoje, 7 de Fevereiro de 2025, o professor de Processamento de Linguagens deu-nos este trabalho para fazer.=OfF E deu-nos 7= dias para o fazer... ON Cada trabalho destes vale 0.25 valores da nota final!"
 
-    tokens = re.findall(r'\d+|[Oo][Nn]|[Oo][Ff][Ff]|=', texto)
-    for token in tokens:
-        if re.fullmatch(r'[Oo][Nn]', token):
-            a_somar = True
-        elif re.fullmatch(r'[Oo][Ff][Ff]', token):
-            a_somar = False
-        elif re.fullmatch(r'\d+', token):
-            if a_somar:
-                ac.append(int(token))
-        elif re.fullmatch(r'=', token):
-            print(sum(ac))
+def somador_on_off(text):
+    soma = 0
+    ativado = True
+    num = ""
+    i = 0
+    text = text.strip().lower()
+    print(text)
 
-texto = input("Indique o texto para realizar a soma: ")
-somador_on_off(texto)
+    for i,c in enumerate(text):
+        if c == "o":
+            if text[i+1] == "n":
+                ativado = True
+            elif text[i+1] == "f" and text[i+2] == "f":
+                ativado = False
+            if num != "":
+                soma += int(num)
+                num = ""
+        elif c.isdigit() and ativado:
+            num += c
+        elif text[i] == "=":
+            print(soma)
+        elif num != "":
+            soma += int(num)
+            num = ""
+    print(soma)
+
+if __name__ == "__main__":
+    somador_on_off(text)
